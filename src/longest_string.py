d4115d0d68ee240ba86f63a01f61eb1b246475e0
26blevine <26blevine@gannacademy.org>
2025-08-29 09:50:35 -0400

    howdydoody

--- src/longest_string.py
+++ src/longest_string.py
@@ -1,6 +1,10 @@
 # Write your solution here
-def longest(list : list):
-    pass
+def longest(strings : list):
+    biggest = ""
+    for str in strings:
+        if len(str) > len(biggest):
+            biggest = str
+    return biggest
 
 if __name__ == "__main__":
     strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
